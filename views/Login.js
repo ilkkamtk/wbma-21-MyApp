252ef3c28519e18ed5d18610c9bbe66ddeb79ce9
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2021-01-25 11:13:51 +0200

    forms

--- views/Login.js
+++ views/Login.js
@@ -31,7 +31,7 @@ const Login = ({navigation}) => {
   }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Login</Text>
       <LoginForm navigation={navigation} />
       <Text>Register</Text>
@@ -39,6 +39,15 @@ const Login = ({navigation}) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 Login.propTypes = {
   navigation: PropTypes.object,
