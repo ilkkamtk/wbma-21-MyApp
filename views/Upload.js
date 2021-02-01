6cf46aa729c420c72130413cdc57129d47db83fe
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2021-02-01 15:20:20 +0200

    begin

--- views/Upload.js
+++ views/Upload.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import {ScrollView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Platform, ScrollView} from 'react-native';
 import {Input, Text, Image, Button, Card} from 'react-native-elements';
 import useUploadForm from '../hooks/UploadHooks';
+import * as ImagePicker from 'expo-image-picker';
 
 const Upload = () => {
+  const [image, setImage] = useState(null);
+
   const {handleInputChange, inputs} = useUploadForm();
+
+  const doUpload = async () => {};
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const {status} = await ImagePicker.requestCameraPermissionsAsync();
+        if (status !== 'granted') {
+          alert(
+            'Sorry, we need camera roll and camera permissions to make this work!'
+          );
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async (library) => {
+    let result = null;
+    const options = {
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 0.5,
+    };
+    if (library) {
+      result = await ImagePicker.launchImageLibraryAsync(options);
+    } else {
+      result = await ImagePicker.launchCameraAsync(options);
+    }
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
 
   return (
     <ScrollView>
       <Text h4>Upload media file</Text>
-      <Image
-        source={{uri: 'http://placekitten.com/400'}}
-        style={{width: '100%', height: undefined, aspectRatio: 1}}
-      />
+      {image && (
+        <Image
+          source={{uri: image}}
+          style={{width: '100%', height: undefined, aspectRatio: 1}}
+        />
+      )}
       <Input
         placeholder="title"
         value={inputs.title}
@@ -23,7 +64,8 @@ const Upload = () => {
         value={inputs.description}
         onChangeText={(txt) => handleInputChange('description', txt)}
       />
-      <Button title="Choose file" />
+      <Button title="Choose from library" onPress={() => pickImage(true)} />
+      <Button title="Use camera" onPress={() => pickImage(false)} />
       <Button title="Upload file" />
     </ScrollView>
   );
