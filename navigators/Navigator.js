d9f03d9c5c1ad2a17f22bee1d4466da1a31205da
mattpe <mattpe@metropolia.fi>
2021-02-12 11:07:34 +0200

    task b ready

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -15,6 +15,7 @@ import {MainContext} from '../contexts/MainContext';
 import {Icon} from 'react-native-elements';
 import Upload from '../views/Upload';
 import MyFiles from '../views/MyFiles';
+import Modify from '../views/Modify';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -60,6 +61,7 @@ const StackScreen = () => {
               headerTitle: getFocusedRouteNameFromRoute(route),
             })}
           />
+          <Stack.Screen name="Modify" component={Modify} />
           <Stack.Screen name="My Files" component={MyFiles} />
           <Stack.Screen name="Single" component={Single} />
         </>
